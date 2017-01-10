51e3a3458570079a9d07b87bf1d170a656a04c50
Michael Micucci <micucci@midokura.com>
2017-01-10 11:40:12 +0900

    Added ability for cURL to use extra headers
    Also added both authentication tokens and the ability to pass in
    extra headers into the curl_cmd utility.

--- bin/curl_cmd.py
+++ bin/curl_cmd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import base64
 from python_utils.net import curl_utils
 
 
@@ -18,21 +19,69 @@ def curl_cmd():
     parser.add_argument('-m', '--media-type', action='store',
                         default='application/json',
                         help='Media type to use', required=False)
+    parser.add_argument('-a', '--auth-url', action='store',
+                        default=None,
+                        help='URL to use to get auth token', required=False)
+    parser.add_argument('-n', '--auth-username', action='store',
+                        default=None,
+                        help='Auth username', required=False)
+    parser.add_argument('-p', '--auth-pass', action='store',
+                        default=None,
+                        help='Auth password', required=False)
+    parser.add_argument('--extra-headers', action='store',
+                        default=None,
+                        help='Extra Headers as a comma-delimited key=value '
+                             'list (no spaces)',
+                        required=False)
+    parser.add_argument('--extra-auth-headers', action='store',
+                        default=None,
+                        help='Extra Headers for authorization as a '
+                             'comma-delimited key=value list (no spaces)',
+                        required=False)
     args = parser.parse_args()
 
     url = args.url
     operation = args.operation
     data = args.data
     mtype = args.media_type
+    auth_url = args.auth_url
+    auth_user = args.auth_username
+    auth_pass = args.auth_pass
+    extra_headers = args.extra_headers
+    extra_auth_headers = args.extra_auth_headers
+    headers = {}
 
     try:
+        token = None
+
+        if auth_url:
+            auth = ('' +
+                    (auth_user if auth_user else '') +
+                    ((':' + auth_pass) if auth_pass else ''))
+
+            auth_headers = {'Authorization': 'Basic ' + base64.b64encode(auth)}
+            if extra_auth_headers:
+                for h in [n.split('=') for n in extra_auth_headers.split(',')]:
+                    auth_headers.update(dict((tuple(h),)))
+
+            token_ret = curl_utils.curl_post(auth_url, json_data={},
+                                             headers=auth_headers)
+
+            headers['X-Auth-Token'] = (
+                curl_utils.curl_get_data_as_dict(token_ret)['key'])
+
+        if extra_headers:
+            for h in [n.split('=') for n in extra_headers.split(',')]:
+                headers.update(dict((tuple(h),)))
 
         if operation == "get":
-            ret = curl_utils.curl_get(url)
+            ret = curl_utils.curl_get(url, headers=headers)
         elif operation == "put":
-            ret = curl_utils.curl_put(url, json_data=data, media_type=mtype)
+            ret = curl_utils.curl_put(url, json_data=data, headers=headers,
+                                      media_type=mtype)
         elif operation == "post":
-            ret = curl_utils.curl_post(url, json_data=data, media_type=mtype)
+            ret = curl_utils.curl_post(url, json_data=data, headers=headers,
+                                       media_type=mtype)
         elif operation == "delete":
             ret = curl_utils.curl_delete(url)
         else:
--- python_utils/net/curl_utils.py
+++ python_utils/net/curl_utils.py
@@ -3,44 +3,55 @@ import pycurl
 from StringIO import StringIO
 
 
-def _curl_do_cmd(url, json_data=None, filename=None,
+def _curl_do_cmd(url, json_data=None, filename=None, headers={},
                  media_type='application/json', custom_request=None):
     cbuffer = StringIO()
     c = pycurl.Curl()
     c.setopt(c.URL, url)
     c.setopt(c.WRITEDATA, cbuffer)
-
     if json_data:
-        c.setopt(c.HTTPHEADER, ["Content-Type: " + media_type])
+        headers["Content-Type: "] = media_type
         c.setopt(c.POSTFIELDS, json.dumps(json_data))
     if filename:
         c.setopt(c.HTTPPOST, [('fileupload', (c.FORM_FILE, file))])
     if custom_request:
         c.setopt(pycurl.CUSTOMREQUEST, custom_request)
+
+    header_array = []
+    for h, v in headers.items():
+        header_array.append(h + ': ' + v)
+
+    c.setopt(c.HTTPHEADER, header_array)
+
     c.perform()
     c.close()
     body = cbuffer.getvalue()
     return body
 
 
-def curl_get(url):
+def curl_get(url, headers={}):
     return _curl_do_cmd(
-        url, json_data=None, filename=None)
+        url, json_data=None, filename=None, headers=headers)
 
 
-def curl_post(url, json_data, filename=None,
+def curl_post(url, json_data, filename=None, headers={},
               media_type='application/json'):
     return _curl_do_cmd(
-        url, json_data=json_data, filename=filename, media_type=media_type)
+        url, json_data=json_data, filename=filename, headers=headers,
+        media_type=media_type)
 
 
-def curl_put(url, json_data=None, filename=None,
+def curl_put(url, json_data=None, filename=None, headers={},
              media_type='application/json'):
     return _curl_do_cmd(
-        url, json_data=json_data, filename=filename, media_type=media_type,
-        custom_request="PUT")
+        url, json_data=json_data, filename=filename, headers=headers,
+        media_type=media_type, custom_request="PUT")
 
 
 def curl_delete(url):
     return _curl_do_cmd(
         url, json_data=None, filename=None, custom_request="DELETE")
+
+
+def curl_get_data_as_dict(json_data):
+    return json.loads(json_data)
